d912a87753fdf3256af4bfb07876333bd74ebc46
juan <juanmmartinss@outlook.com>
2023-10-07 13:34:31 -0300

    atualizações Manna

--- item1/Manna-Pnueli.c
+++ item1/Manna-Pnueli.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <omp.h>
+
+void client(int id);
+void server(int id);
 
 int main(){
 
-    
+
     return 0;
+}
+
+void client(int id){
+
+}
+
+void server(int id){
 }
